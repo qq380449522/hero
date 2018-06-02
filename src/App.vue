f27c02a47feb858b158554a6637320b4db960df2
qin <16631137667@163.com>
2018-06-02 08:44:09 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <myheader></myheader>
+    <div class="container-fluid">
+    <div class="row">
+      <mynav></mynav>
+      <myList></myList>
+    </div>
+    </div>
+  </div>
+</template>
+<script>
+import myheader from '@/components/myHeader.vue'
+import mynav from '@/components/myNav.vue'
+import myList from '@/components/myList.vue'
+export default {
+  name: 'App',
+  components: {
+    myheader,
+    mynav,
+    myList
+  }
+}
+</script>
+
+<style>
+</style>
